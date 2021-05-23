f2e0ad9fb4a63d1fca596af6acca04206fc1e25e
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2021-05-23 02:01:13 -0300

    configuração da porta do servidor

--- index.js
+++ index.js
@@ -49,6 +49,8 @@ app.post('/message', (req, res) => {
 
 });
 
-app.listen(3000, function () {
-    console.log('Servidor ativo na porta 3000!');
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+    console.log(`Servidor ativo na porta ${port}!`);
 })
